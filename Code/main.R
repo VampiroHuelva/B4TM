87d4949a3a56192413c82af503ffaa5581fbb7af
b_daa <d2.bijkerk@student.vu.nl>
2017-05-04 22:37:43 +0200

    argue

--- Code/main.R
+++ Code/main.R
@@ -1,7 +1,20 @@
+#######################################################################################
+###
+### Script to select, make and train a model for the classification of different
+### cancer types. First the data is read in to perform a data inspection.... TODO FINISH  
+### .....
+### 
+### Dependecies: misc.R
+### input: ... datasets
+### output: .... models
+###
+###
+########################################################################################
+
 # In this script we do a comples analysis of the data with a simple feature
 # selection and adding a vector with a vector selection used in weka.
 # Algorithm used will be on the report. We use this script to first select
-# the number of features is optimus for each algorithm, and then select the best algortihm
+# the number of features is optimus for each algorithm, and then select the best algorithm
 
 ### TODO the above needs to be more clear 
 
@@ -10,27 +23,15 @@ rm(list = ls())
 source("code/misc.R")
 
 # Loading all the dependicies
-library('getopt')
-library('caret')
-library(splines)
-library(parallel)
-library(survival)
-library(caret)
-library(mlbench)
-library(gbm)
-library(corrplot)
-library(pROC)
-library(FSelector)
-library(qtlcharts)
-library(lattice)
-library(energy)
-library(RWeka)
+library(splines); library(parallel); library(survival); library(caret); library(mlbench)
+library(gbm); library(corrplot); library(pROC); library(FSelector); library(qtlcharts)
+library(lattice); library(energy); library(RWeka); 
 
 # get the train data and write to WEKA file
 combine = Load_labeled_Data()
 #write.arff(combine, file = "combine.arff")
 
-#################### FEATURE SELECTION ####################################
+################################## FEATURE SELECTION ####################################
 
 # Feature selection by importance ()
 features = filter_Var_selection()
@@ -48,7 +49,7 @@ filtering <- c("V312","V673","V696","V773","V1570","V1657","V2185","V25932","V26
 Huge_combination <- c(J48_previous, CorrelationAttribute[1:25], filtering, features[1:20])
 Huge_Unic <- unique(Huge_combination)
 
-#################### GENERATING TEST/TRAIN SETS ###############################
+############################### GENERATING TEST/TRAIN SETS ###############################
 
 # create simple cross validation sets (works only with 3 classes)
 simple_cross_sets = crossval_sets(combine, crossval = 5) 
@@ -64,43 +65,105 @@ HER2 = make_equal_sets(two_class_data[[1]])
 HR = make_equal_sets(two_class_data[[2]])
 Triple = make_equal_sets(two_class_data[[3]])
 
-# specify the models to be trained                                    
-models = data.frame("nnet" = 1:10, "rf"=1:10, "gbm" = 1:10, "glm" = 1:10)
-models
+############################## TRAINING MODELS ############################################
+
+## model selection (store the settings for models to train with)##
+models = c("nnet", "rf", "gbm", "glm")
 
 ### (TODO specify above the proper settings for every trained model)
 
-##################### TRAINING MODELS ############################################
+# make list for storing the trained models
+trained_models <- vector("list",4)
 
-
-#### working on this ttonigt (daan)
-
-models <- vector("list",crossval)
-
-fitControl <- trainControl(## 10-fold CV
-  method = "repeatedcv",
-  number = 10,
-  ## repeated ten times
-  repeats = 10)
-features = Huge_Unic[1:12]
-model = paste(features, collapse=' + ')
-model = paste("Subgroup ~ ", model, collapse ='')
-MODEL <- train(eval(parse(text=model)), data = combine, method = "nnet", trControl = fitControl, verbose = FALSE)
-
-
-NNET_Model <- function(){
-  fitControl <- trainControl(## 10-fold CV
-    method = "repeatedcv",
-    number = 10,
-    ## repeated ten times
-    repeats = 10)
-  features = Huge_Unic[1:12]
-  model = paste(features, collapse=' + ')
-  model = paste("Subgroup ~ ", model, collapse ='')
-  MODEL <- train(eval(parse(text=model)), data = combine, method = "nnet", trControl = fitControl, verbose = FALSE)
+## TRAIN without double loop featureselection
+for (i in 1:length(models)) {
+  # TODO specify for each model certain properties (number of features for instance)
+  
+  # the below SHOULD actually be a double loop (feature selection on each crossval train/test set)
+  for (j in 1:feature_selection) {
+    
+    # do feature selection[j] (TODO)
+    features = Huge_Unic[1:12]
+    
+    # set spefic settings for model
+    fitControl <- trainControl(## 10-fold CV
+      method = "repeatedcv",
+      number = 10,
+      ## repeated ten times
+      repeats = 10) 
+    
+    # makeformula 
+    formula_model = paste(features, collapse=' + ')
+    formula_model = paste("Subgroup ~ ", model, collapse ='')
+    
+    ### TODO specify which data to use sets!!! and apply setttings for specific model
+    model <- train(eval(parse(text=model)), data = combine, method = models[i], trControl = fitControl, verbose = FALSE)
+    # save model
+  }
 }
 
 
+
+## TRAIN: here i try the double loop crossval feature selection
+## DO THE FOLOWWING FOR ALL FEATURE SELECTION METHODS
+
+trained_models <- vector("list",4) 
+
+for (i in 1:length(models)) {
+  # TODO specify for each model certain properties (number of features for instance)
+  
+  # make a list for the predictions on the test sets and for the real values in the test set
+  pred_test = c()
+  real_values = c()
+  
+  # feature selection on each crossval train and do test and store predictions for roc plot
+  for (j in 1:crossval) {
+    
+    # do feature selectionA (TODO choose feature selection function)
+    features = Huge_Unic[1:12]
+    
+    # set spefic settings for model
+    fitControl <- trainControl(## 10-fold CV
+      method = "repeatedcv",
+      number = 10,
+      ## repeated ten times
+      repeats = 10) 
+    
+    # makeformula 
+    formula_model = paste(features, collapse=' + ')
+    formula_model = paste("Subgroup ~ ", model, collapse ='')
+    
+    model <- train(eval(parse(text=model)), data = trainset[i], method = models[i], trControl = fitControl, verbose = FALSE)
+    # save model
+    
+    # add real and predicted values
+    real = append(real, testset)      
+    pred = append(pred(predict(model,testset[j])))
+  }
+  
+  # save for each crossval the model, predictions and real classes
+  trained_models[[i]] <- list(model, real, pred)
+  
+}
+
+
+##################### NOW we can compare the models with different feature selection ###################
+
+for (i in 1:4) {
+    r = c()
+    print(confusionMatrix(real[i], pred[i])[2:3])
+    x = as.numeric(sets[[i]][[1]][[j]][[2]][,1])
+    y = as.numeric(pred)
+    roc = multiclass.roc(x,y)
+    r <- append(r,auc(roc))
+    rs <- roc[['rocs']]
+    plot.roc(rs[[1]])
+    sapply(2:length(rs),function(h) lines.roc(rs[[h]],col=h))
+}
+
+####################### NOW we should train 4 models with best features selected on cross val ###########
+
+# compare the 4 models with each other and do the final predictions (TODO)
 
 #################################### DO THE FINAL PREDICTIONS ####################################
 
